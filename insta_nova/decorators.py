fcee6c460482dd4958e3d2ab7681895da058dc2a
Lavanya Mishra <lavanyamishra1996@gmail.com>
2025-07-13 20:06:45 +0530

    feat: add validator for get_access_token method.

--- insta_nova/decorators.py
+++ insta_nova/decorators.py
@@ -2,6 +2,8 @@ from functools import wraps
 from .validators import (
     validate_app_id,
     validate_app_secret,
+    validate_authorization_code,
+    validate_redirect_uri,
 )
 
 def validate_set_application_credentials(func):
@@ -11,3 +13,12 @@ def validate_set_application_credentials(func):
         validate_app_secret(app_secret)
         return func(cls, app_id, app_secret)
     return wrapper
+
+def validate_get_access_token(func):
+    @wraps(func)
+    def wrapper(self, authorization_code, redirect_uri):
+        validate_authorization_code(authorization_code)
+        validate_redirect_uri(redirect_uri)
+        return func(self, authorization_code, redirect_uri)
+    return wrapper
+
